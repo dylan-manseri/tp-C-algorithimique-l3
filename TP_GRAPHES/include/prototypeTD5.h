b78a65285981c30db20ed77c5ad430f222d68278
Dylan Manseri <manseri.dylan1@gmail.com>
2026-03-02 22:34:29 +0100

    td5 fermeture transitive, fortement connexe

--- TP_GRAPHES/include/prototypeTD5.h
+++ TP_GRAPHES/include/prototypeTD5.h
@@ -9,6 +9,10 @@
 
 void reParcoursProfondeurCC(int *s, int* visite, void* g, TypeGraphe type, int n, int* nbSomVisite, int* CC, int* nbCC);
 int* getCC(void* g, TypeGraphe type);
+void reParcoursProfondeurSuffixe(int s, Pile* p, int* nbSomVisite, void* g, int* visite, int n, TypeGraphe type);
+void reParcoursProfondeurCFC_T(int s, int* nbSomVisite, void* g, int* visite, int n, int nbCFC, int* CFC, TypeGraphe type);
+int* getCFC(void* g, TypeGraphe type);
+void fermetureTransitive(void* g, TypeGraphe type, int** mat);
 
 int mainTD5(void);
 
